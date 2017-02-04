f2881f17c5f4f762d003621996e28cf03feec944
Michael Kulinski <me@michaelkulinski.com>
2017-02-03 19:34:23 -0800

    fetching from giphy api

--- src/actions/catGifActions.js
+++ src/actions/catGifActions.js
@@ -1,13 +1,28 @@
 import * as types from '../constants/actionTypes';
 
-export function grabGifUrl(url) {
-  return function (dispatch) {
-    return dispatch({
-      type: types.ADD_GIF,
-      payload: url,
-    });
+
+export function grabGifUrl() {
+  return (dispatch, getState) => {
+    const state = getState();
+    return fetch('http://api.giphy.com/v1/gifs/search?q=cat&api_key=dc6zaTOxFJmzC')
+      .then(response => response.json())
+      .then(json => {
+        console.log('json', json);
+        // console.log('currUrl', state.catGifs.currUrl);
+        // console.log('playedUrls', state.catGifs.playedUrls);
+        // dispatch({ type: types.ADD_GIF, payload: response.data });
+      })
   };
 }
+
+// export function grabGifUrl() {
+//   return function (dispatch) {
+//     return dispatch({
+//       type: types.ADD_GIF,
+//       payload: url,
+//     });
+//   };
+// }
 
 // export function checkIfInPlayedUrls(oldUrl) {
 //   return {
--- src/containers/CatGifsPage.js
+++ src/containers/CatGifsPage.js
@@ -2,18 +2,19 @@ import React, { PropTypes } from 'react';
 import Gif from '../components/Gif';
 
 
-export const CatGifsPage = (props) => {
+const CatGifsPage = (props) => {
   console.log(props);
-  const { catGifs } = props;
+  const { catGifs, actions } = props;
   const handleClick = () => {
     console.log('clicked');
     // run get request to giphy
+    actions.grabGifUrl();
   };
 
   return (
     <div>
       <h1>Cat Gif Page</h1>
-      <Gif url={props.catGifs.currUrl} />
+      <Gif url={catGifs.currUrl} />
       <button onClick={handleClick}>
         New Giphy
       </button>
